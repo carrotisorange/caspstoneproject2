78247113f9b1a11153125c99df297deb065fe6f3
= <=>
2021-06-11 22:18:01 +0800

    upload the screening application.

---
+++ app.py
@@ -0,0 +1,62 @@
+# import all the packages
+from flask import Flask, render_template, request
+from googletrans import Translator
+import pandas as pd
+import functions as function
+
+translator = Translator()
+
+app = Flask(__name__)
+
+
+# 1. route for the search page
+@app.route('/')
+def search():
+    return render_template('pages/index.html')
+
+
+# 2. route for the screening
+@app.route('/screen', methods=['POST'])
+def screen():
+    # 1. retrieve the input from the input field
+    x = request.form['input']
+
+    # 2. preprocess the input
+    preprocessed_input = function.preprocess_input(x)
+
+    # 3. validate the input
+    validated_input = function.validate_input(preprocessed_input)
+
+    if validated_input == 'rejectNumerical':
+        return render_template('pages/index.html', error_message="Your input is not valid. Please enter a string.",
+                               product_idea=preprocessed_input)
+    elif validated_input == 'rejectShortText':
+        return render_template('pages/index.html', error_message="Your input is too short. Try longer string.",
+                               product_idea=preprocessed_input)
+    elif validated_input == 'languageNotSupported':
+        return render_template('pages/index.html',
+                               error_message="Your language is not supported. Please enter a Tagalog/English "
+                                             "string "
+                                             "only.", product_idea=preprocessed_input)
+
+    classified_input = function.classify_input(preprocessed_input)
+
+    return render_template("pages/screen.html", classification=classified_input, product_idea=preprocessed_input)
+
+
+# 3. routes for dashboard page
+@app.route('/dashboard')
+def dashboard():
+    # datasets with complete complete product ideas
+    product_ideas = pd.read_csv("C://Users//hp user//Desktop//caspstoneproject2//dataset//dataset.csv")
+    product_ideas.dropna(subset=['product_idea'], inplace=True)
+
+    # 5. get the top words based on frequency
+    top_new_product_ideas = function.get_the_top_product_ideas(product_ideas)
+
+    return render_template("pages/dashboard.html",
+                           top_new_product_ideas=top_new_product_ideas.to_dict(orient='records'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,72 @@
+# import all the packages
+from flask import render_template
+import preprocess_kgptalkie as ps
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+from googletrans import Translator
+import pickle
+from collections import Counter
+from wordcloud import WordCloud, STOPWORDS
+
+translator = Translator()
+
+
+# 1. function to preprocess the input
+def preprocess_input(x):
+    x = str(x).lower().replace('\\', '').replace('_', ' ')
+    x = ps.remove_urls(x)
+    x = ps.remove_html_tags(x)
+    # x = ps.remove_accented_chars(x)
+    x = ps.remove_special_chars(x)
+    x = re.sub("(.)\\1{2,}", "\\1", x)
+    # x = ''.join([i for i in x if not i.isdigit()])
+    return x
+
+
+# 2. function to validate the input
+def validate_input(preprocessed_input):
+    error = ""
+    # reject numerical input
+    if preprocessed_input.isdigit():
+        error = "rejectNumerical"
+        return error
+    # reject input with less than 3 characters
+    elif len(preprocessed_input) < 3:
+        error = "rejectShortText"
+        return error
+    else:
+        # extract the language of the text
+        product_idea_lang = translator.detect(preprocessed_input).lang
+
+        # condition for the language of text
+        if product_idea_lang == 'en':
+            validated_input = ps.make_base(preprocessed_input)
+            return validated_input
+        elif product_idea_lang == 'tl':
+            translated_input = ps.make_base(translator.translate(preprocessed_input).text)
+            return translated_input
+        else:
+            error = 'languageNotSupported'
+            return error
+
+
+# 3. function to classify the input
+def classify_input(validated_input):
+    classifier_f = open("C://Users//hp user//Desktop//caspstoneproject2//model//model.pickle", "rb")
+    clf = pickle.load(classifier_f)
+    classifier_f.close()
+    classification = clf.predict([validated_input])
+    if classification == [1]:
+        result = '1'
+    else:
+        result = '0'
+
+    return result
+
+
+# 4. function to get the top words based on interaction count
+def get_the_top_product_ideas(product_ideas):
+    results = product_ideas[product_ideas['label'] == 1]
+
+    return results
